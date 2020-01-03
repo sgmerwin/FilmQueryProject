56b57d0a62ec1b7ded512fc4c8c26142b01da858
sgmerwin <stevegmerwin@gmail.com>
2020-01-03 16:45:45 -0700

    initial commit

---
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -0,0 +1,21 @@
+package com.skilldistillery.filmquery.database;
+
+import com.skilldistillery.filmquery.entities.Film;
+
+public class DatabaseAccessorObject implements DatabaseAccessor {
+	
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+  @Override
+  public Film findFilmById(int filmId) {
+    return null;
+  }
+
+}
