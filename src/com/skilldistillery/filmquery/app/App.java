491afcda62717adca3d334d0700d972eade37b59
sgmerwin <stevegmerwin@gmail.com>
2020-01-05 08:37:47 -0700

    correct indents

--- src/com/skilldistillery/filmquery/app/App.java
+++ src/com/skilldistillery/filmquery/app/App.java
@@ -12,7 +12,7 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class App {
-	
+
 	public static Scanner input;
 	public static DatabaseAccessorObject db;
 	public static int choice;
@@ -25,94 +25,94 @@ public class App {
 	public static String[] parts;
 	public static int count = 0;
 	public static Boolean exit = false;
-	
+
 	public void menu() throws SQLException {
 		while(exit == false) {
-		db = new DatabaseAccessorObject();
-		input = new Scanner(System.in);	
-		System.out.println();
-		System.out.println("1. Look up a film by id");
-		System.out.println("2. Look up a film by a search keyword");
-		System.out.println("3. Exit the application");
-		System.out.println();
-		System.out.println("Enter an integer choice");
-		choice = input.nextInt();
-		if(choice == 1) {
-			choice_1();
-		}
-		if(choice == 2) {
-			choice_2();
-		}
-		if(choice == 3) {
-			choice_3();
-		}
-		else {
-			choice_4();
-		}
+			db = new DatabaseAccessorObject();
+			input = new Scanner(System.in);	
+			System.out.println();
+			System.out.println("1. Look up a film by id");
+			System.out.println("2. Look up a film by a search keyword");
+			System.out.println("3. Exit the application");
+			System.out.println();
+			System.out.println("Enter an integer choice");
+			choice = input.nextInt();
+			if(choice == 1) {
+				choice_1();
+			}
+			if(choice == 2) {
+				choice_2();
+			}
+			if(choice == 3) {
+				choice_3();
+			}
+			else {
+				choice_4();
+			}
 		}//while
 	}//menu
-	
+
 	public void choice_1() throws SQLException {
 		while(exit == false) {
-		System.out.println("Enter a film id");
-		id = input.nextInt();
-		db.findFilmById(id);
-		menu();
+			System.out.println("Enter a film id");
+			id = input.nextInt();
+			db.findFilmById(id);
+			menu();
 		}//while
 	}//choice_1
-	
+
 	public void choice_2() throws SQLException {
 		while(exit == false) {
-		System.out.println();	
-		System.out.println("Enter a search keyword"); 
-		  str = input.next();
-		  conn = DriverManager.getConnection(db.getURL(), db.getUser(),db.getPass()); 
-		  sql = "select id, title, description from film";
-		  stmt = conn.prepareStatement(sql); 
-		  rs =stmt.executeQuery(); 
-		  while (rs.next()) {
-		  //System.out.println(rs.getString("title") + " "+rs.getString("description")); 
-		  parts = rs.getString("title").split("\\s+"); 
-		  for(String i : parts) {
-		  if(str.equalsIgnoreCase(i)) { 
-			  id = Integer.parseInt(rs.getString("id"));
-			  db.findFilmById(id);
-			  ++count;
-			  }//if 
-		  }//for 
-		  parts = rs.getString("description").split("\\s+"); 
-		  for(String i : parts) {
-		  if(str.equalsIgnoreCase(i)) { 
-			  id = Integer.parseInt(rs.getString("id"));
-			  db.findFilmById(id); 
-			  ++count; 
-			  }//if 
-		  }//for 
-		  }//while 
-		  if(count == 0) {
-		  System.out.println("No matches in the database"); 
-		  }//if
-		  menu();
+			System.out.println();	
+			System.out.println("Enter a search keyword"); 
+			str = input.next();
+			conn = DriverManager.getConnection(db.getURL(), db.getUser(),db.getPass()); 
+			sql = "select id, title, description from film";
+			stmt = conn.prepareStatement(sql); 
+			rs =stmt.executeQuery(); 
+			while (rs.next()) {
+				//System.out.println(rs.getString("title") + " "+rs.getString("description")); 
+				parts = rs.getString("title").split("\\s+"); 
+				for(String i : parts) {
+					if(str.equalsIgnoreCase(i)) { 
+						id = Integer.parseInt(rs.getString("id"));
+						db.findFilmById(id);
+						++count;
+					}//if 
+				}//for 
+				parts = rs.getString("description").split("\\s+"); 
+				for(String i : parts) {
+					if(str.equalsIgnoreCase(i)) { 
+						id = Integer.parseInt(rs.getString("id"));
+						db.findFilmById(id); 
+						++count; 
+					}//if 
+				}//for 
+			}//while 
+			if(count == 0) {
+				System.out.println("No matches in the database"); 
+			}//if
+			menu();
 		}//while
 	}//choice_2
-	
+
 	public void choice_3() {
 		while(exit == false) {
-		System.out.println("Exiting Application");
-		exit = true;
+			System.out.println("Exiting Application");
+			exit = true;
 		}
 	}//choice_3
-	
+
 	public void choice_4() throws SQLException {
 		System.out.println("Enter an integer 1, 2, or 3");
 		menu();
 	}
-	
+
 	//story 1
 	public static void main(String[] args) throws SQLException {
-		
+
 		App app = new App();
 		app.menu();
-		
+
 	}//main
 }//class
